ded254183d9621baf5fc862f2da1efdd4dc4234a
David <fifth.drink@gmail.com>
2025-10-11 13:33:09 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,29 @@
 self.addEventListener('install', (event) => {
+  event.waitUntil(self.addEventListener('install', (event) => {
   event.waitUntil(
+    caches.open('zenvibe-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/public/ZenVibeContent.json',
+        '/public/bell1.mp3',
+        '/public/gong2.mp3',
+        '/public/chime3.mp3',
+        '/public/icon.png',
+        '/assets/main.js',
+        '/assets/main.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
     caches.open('zenvibe-cache').then((cache) => {
       return cache.addAll([
         '/',
